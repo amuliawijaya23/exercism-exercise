38ddb5f6d31a14681bf48c9a6a6c7733a8ac0e2a
amuliawijaya23 <amuliawijaya@gmail.com>
2022-11-01 01:14:52 +0000

    completes testing for canSpy function

--- test/annalyns-infiltrations-test.js
+++ test/annalyns-infiltrations-test.js
@@ -1,6 +1,12 @@
 const { canExecuteFastAttack, canSpy, canSignalPrisoner, canFreePrisoner } = require('../annalyns-infiltration');
 const assert = require('chai').assert;
 
+/*
+  Tests the canExecuteFastAttack function
+  takes the knightIsAwake parameter
+  returns a boolean
+*/
+
 describe('#canExecuteFastAttack', () => {
   it('should return false if knight is awake', () => {
     assert.strictEqual(canExecuteFastAttack(true), false);
@@ -9,4 +15,29 @@ describe('#canExecuteFastAttack', () => {
     assert.strictEqual(canExecuteFastAttack(false), true);
   });
 });
+
+/*
+  Tests the canSpy function.
+  takes the knightIsAwake, archerIsAwake, prisonerIsAwake parameter
+  returns a boolean
+*/
+
+describe('#canSpy', () => {
+  it('should return false if everyone is asleep', () => {
+    assert.strictEqual(canSpy(false, false, false), false);
+  });
+  it('should return true if knight is awake, archer and prisoner are asleep', () => {
+    assert.strictEqual(canSpy(true, false, false), true);
+  });
+  it('should return true if archer is awake, knight and prisoner are asleep', () => {
+    assert.strictEqual(canSpy(false, true, false), true);
+  });
+  it('should return true if prisoner is awake, knight and archer are asleep', () => {
+    assert.strictEqual(canSpy(false, false, true), true);
+  });
+  it('should return true if knight and archer are awake, prisoner is asleep', () => {
+    assert.strictEqual(canSpy(true, true, false), true);
+  });
+});
+
 
