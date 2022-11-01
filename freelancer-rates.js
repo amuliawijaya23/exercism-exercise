33a18c2c40c4806c429b668083a594a1a59a617a
amuliawijaya23 <amuliawijaya@gmail.com>
2022-11-01 19:52:40 +0000

    implements function

--- freelancer-rates.js
+++ freelancer-rates.js
@@ -1,13 +1,19 @@
-const dayRate = () => {
-
+const dayRate = (hourlyRate) => {
+  return hourlyRate * 8;
 };
 
-const daysInBudget = () => {
-
+const daysInBudget = (fixedBudget, hourlyRate) => {
+  return Math.floor(fixedBudget / (hourlyRate * 8));
 };
 
-const priceWithMonthlyDiscount = () => {
+const priceWithMonthlyDiscount = (hourlyRate, numberOfDays, discountRate) => {
+  const remainingDays = numberOfDays % 22;
+  const daysInMonths = numberOfDays = remainingDays;
 
+  const discountedRate = ((1 - discountRate) * hourlyRate) * (daysInMonths * 8);
+  const proRatedRate = remainingDays * (hourlyRate * 8);
+
+  return Math.ceil((discountedRate + proRatedRate));
 };
 
 module.exports = {
