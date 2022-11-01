ad14b062140f1e8f8bd0915bd7549401b1478976
amuliawijaya23 <amuliawijaya@gmail.com>
2022-11-01 20:35:56 +0000

    wrote test for frontDoorResponse function

--- test/poetry-club-door-policy-test.js
+++ test/poetry-club-door-policy-test.js
@@ -1,2 +1,20 @@
 const { frontDoorResponse, frontDoorPassword, backDoorResponse, backDoorPassword } = require('../poetry-club-door-policy');
 const assert = require('chai').assert;
+
+describe('#frontDoorResponse', () => {
+  it('Returns the first character of a given line', () => {
+    assert.strictEqual(frontDoorResponse('I won\'t give up'), 'I');
+  });
+});
+
+describe('frontDoorPassword', () => {
+  
+});
+
+describe('backDoorResponse', () => {
+
+});
+
+describe('backDoorPassword', () => {
+
+});
