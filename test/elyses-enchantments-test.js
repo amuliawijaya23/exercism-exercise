4267424b792ee153c14b944e77723c0610f69120
amuliawijaya23 <amuliawijaya@gmail.com>
2022-11-02 22:46:04 +0000

    set up testing for elyses-enchantments exercise

---
+++ test/elyses-enchantments-test.js
@@ -0,0 +1,11 @@
+const {
+  getItem,
+  setItem,
+  insertItemAtTop,
+  removeItem,
+  removeItemFromTop,
+  insertItemAtBottom,
+  removeItemAtBottom,
+  checkSizeOfStack
+} = require('../elyses-enchantments');
+const assert = require('chai').assert;
