3b442875b267acc678cb10b2e40ada9fddb26a77
amuliawijaya23 <amuliawijaya@gmail.com>
2022-11-02 22:44:25 +0000

    set up for elyses enchantments exercise

---
+++ elyses-enchantments.js
@@ -0,0 +1,42 @@
+const getItem = (cards, position) => {
+
+};
+
+const setItem = (cards, position, replacementCard) => {
+
+};
+
+const insertItemAtTop = (cards, newCard) => {
+
+};
+
+const removeItem = (cards, position) => {
+
+};
+
+const removeItemFromTop = (cards) => {
+
+};
+
+const insertItemAtBottom = (cards, newCard) => {
+
+};
+
+const removeItemAtBottom = (cards) => {
+
+};
+
+const checkSizeOfStack = (cards, stackSize) => {
+
+};
+
+module.exports = {
+  getItem,
+  setItem,
+  insertItemAtTop,
+  removeItem,
+  removeItemFromTop,
+  insertItemAtBottom,
+  removeItemAtBottom,
+  checkSizeOfStack
+}
